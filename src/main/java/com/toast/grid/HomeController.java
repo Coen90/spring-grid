81f5ec0bae10393f45fc82739146af13d97d7393
Hyuntae <bht9011@gmail.com>
2022-04-12 22:59:03 +0900

    spring security practice with toast ui grid

--- src/main/java/com/toast/grid/HomeController.java
+++ src/main/java/com/toast/grid/HomeController.java
@@ -17,30 +17,24 @@ import lombok.extern.slf4j.Slf4j;
  * Handles requests for the application home page.
  */
 @Controller
-@Slf4j
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
-	@RequestMapping(value = "/build", method = RequestMethod.GET)
-	public String build(Locale locale, Model model) {
-		
-		return "builder";
-	}
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
+    }
+
+    @RequestMapping(value = "/build", method = RequestMethod.GET)
+    public String build(Locale locale, Model model) {
+
+        return "builder";
+    }
 }
